9447db0dd85f4bd95bcc68947f9e383d7d504799
schemr <schemryj@gmail.com>
2018-01-18 12:32:06 +0900

    Added redux action, reducer

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
-AppRegistry.registerComponent('firstreactnative', () => App);
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const AppRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent('firstreactnative', () => AppRedux);
