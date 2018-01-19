bd1282373bc061797c9a897106c9285f10360299
schemr <schemryj@gmail.com>
2018-01-19 18:32:59 +0900

    Changed input, heading text to ui component

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import InputStyle from '../../components/UI/InputStyle/InputStyle';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -10,12 +12,33 @@ class AuthScreen extends Component {
 
     render () {
         return (
-            <View>
-                <Text>Auth Screen</Text>
+            <View style={styles.container}>
+                <HeadingText>Login</HeadingText>
+                <Button title="Switch to login" />
+                <View style={styles.inputContainer}>
+                    <InputStyle style={styles.input} placeholder="Email Address" />
+                    <InputStyle style={styles.input} placeholder="Password" />
+                    <InputStyle style={styles.input} placeholder="Confirm Password" />
+                </View>
                 <Button title="Login" onPress={this.loginHandler}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputContainer: {
+        width: '80%'
+    },
+    input: {
+        backgroundColor: '#eee',
+        borderColor: '#bbb'
+    }
+})
 
 export default AuthScreen;
