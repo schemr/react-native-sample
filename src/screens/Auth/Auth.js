17c7efd3f5f28e849b005b44304aa2dd0f2cdd1a
schemr <schemryj@gmail.com>
2018-01-21 18:46:14 +0900

    Added responsive design in auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import InputStyle from '../../components/UI/InputStyle/InputStyle';
@@ -9,22 +9,49 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 import BackgroundImg from '../../assets/images/background.jpg';
 
 class AuthScreen extends Component {
+    state = {
+        viewMode: Dimensions.get('window').height>500 ? 'portrait' : 'landscape'
+    }
+    constructor(props) {
+        super(props);
+        Dimensions.addEventListener("change", this.updateStyles);
+    }
+    componentWillUnmount() {
+        Dimensions.removeEventListener("change", this.updateStyles);
+    }
+    updateStyles = (dims) => {
+        this.setState({
+            viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
+        })
+    }
     loginHandler = () => {
         startMainTabs();
     }
 
     render () {
+        let headingText = null;
+        if(this.state.viewMode === 'portrait'){
+            headingText = (
+                <MainText>
+                    <HeadingText>Login</HeadingText>
+                </MainText>
+            )
+        }
         return (
             <ImageBackground source={BackgroundImg} style={styles.backgroundImg}>
                 <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Login</HeadingText>
-                    </MainText>
+                    {headingText}
                     <ButtonWithBackground color="#29aaf4">Switch to login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
                         <InputStyle style={styles.input} placeholder="Email Address" />
-                        <InputStyle style={styles.input} placeholder="Password" />
-                        <InputStyle style={styles.input} placeholder="Confirm Password" />
+                        <View style={this.state.viewMode === 'portrait' ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
+                        <View style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
+                            <InputStyle style={styles.input} placeholder="Password" />
+                        </View>
+                        <View style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
+                            <InputStyle style={styles.input} placeholder="Confirm Password" />
+                        </View>
+                        </View>
                     </View>
                     <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Login</ButtonWithBackground>
                 </View>
@@ -49,6 +76,20 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: '#eee',
         borderColor: '#bbb'
+    },
+    landscapePasswordContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    portraitPasswordContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start'
+    },
+    landscapePasswordWrapper: {
+        width: '45%'
+    },
+    portraitPasswordWrapper: {
+        width: '100%'
     }
 })
 
