49675ed7751cc5a1a158f5fb534f69416ef1d18a
schemr <schemryj@gmail.com>
2018-01-21 17:20:17 +0900

    Styling side menu (logout)

--- src/screens/SideMenu/SideMenu.js
+++ src/screens/SideMenu/SideMenu.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 class SideMenu extends Component {
     render() {
         return (
             <View style={[styles.container, {width: Dimensions.get("window").width * 0.8}]}>
-                <Text>Side Menu</Text>
+                <TouchableOpacity>
+                    <View style={styles.menuItem}>
+                        <Icon name="ios-log-out" size={30} color="#aaa" style={styles.menuIcon} />
+                        <Text>Sign Out</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -13,9 +19,18 @@ class SideMenu extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 22,
+        paddingTop: 50,
         backgroundColor: "white",
         flex: 1
+    },
+    menuItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#eee'
+    },
+    menuIcon: {
+        marginRight: 10
     }
 })
 
