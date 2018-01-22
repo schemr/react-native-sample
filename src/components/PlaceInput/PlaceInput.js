40504eb225ece021ed82af2ba88b0d6509f05d46
schemr <schemryj@gmail.com>
2018-01-22 17:21:21 +0900

    Changed share place input to input handling

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -3,7 +3,13 @@ import { View, TextInput, Button } from "react-native";
 import InputStyle from '../UI/InputStyle/InputStyle';
 
 const placeInput = props => (
-  <InputStyle placeholder="Place Name" value={props.placeName} onChangeText={props.onChangeText}/>
+  <InputStyle 
+    placeholder="Place Name" 
+    value={props.placeData.value}
+    valid={props.placeData.valid}
+    touched={props.placeData.touched} 
+    onChangeText={props.onChangeText}
+    />
 );
 
 export default placeInput;
