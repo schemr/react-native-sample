5620fdbced1e3332809ad6473b76d1179ee7a60e
schemr <schemryj@gmail.com>
2018-01-24 15:16:42 +0900

    Added map into pick location

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet} from 'react-native';
+import { View, Text, Button, StyleSheet, Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
 
 class PickLocation extends Component {
+    state = {
+        focusedLocation: {
+            latitude: 37.7900352,
+            longitude: -122.4013726,
+            latitudeDelta: 0.0122,
+            longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.placeholder}>
-                    <Text>Map</Text>
-                </View>
+                <MapView 
+                    initialRegion={this.state.focusedLocation}
+                    style={styles.map} />
                 <View style={styles.btnContainer}>
                     <Button title="Locate Me" onPress={()=>alert('locate')} />
                 </View>
@@ -21,12 +30,9 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'center'
     },
-    placeholder: {
-        borderWidth: 1,
-        borderColor: 'black',
-        backgroundColor: '#eee',
-        width: '80%',
-        height: 150
+    map: {
+        width: '100%',
+        height: 250
     },
     btnContainer: {
         margin: 8
