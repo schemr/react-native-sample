7e7d42ad73a5ccd0b7df43472b7d9a0f5b6138d8
schemr <schemryj@gmail.com>
2018-01-28 13:49:08 +0900

    Setting max-width, max-height of upload image

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -8,7 +8,7 @@ class PickImage extends Component {
     }
 
     pickImageHandler = () => {
-        ImagePicker.showImagePicker({title: "Pick an image"}, res => {
+        ImagePicker.showImagePicker({title: "Pick an image", maxWidth: 800, maxHeight: 600 }, res => {
             if(res.didCancel) {
                 console.log("User cancelled!");
             }else if(res.error) {
